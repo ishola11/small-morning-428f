a3f3a70b0dcba0b5502d360b350bc9142a726938
ishola11 <76501353+ishola11@users.noreply.github.com>
2025-03-08 03:45:04 -0500

    Updated to Under Maintenace

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Website Under Maintenance";
+export const SITE_DESCRIPTION = "We're currently working on updates. Please check back soon!";
